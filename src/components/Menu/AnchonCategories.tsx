05bed8799d42f131c2d93bed7ba62306b1b87b9f
SergioP <e.desauditcorp1.tid@telefonica.com>
2021-03-14 21:06:28 +0100

    REFACTOR: Aupa Anchon componente de Bilbao

---
+++ src/components/Menu/AnchonCategories.tsx
@@ -0,0 +1,30 @@
+import {
+    Tabs,
+    Select,
+    Anchor,
+
+} from "antd";
+import {restaurant_menu} from '../../Models/instances'
+const { Link: AnchorLink } = Anchor;
+const { TabPane } = Tabs;
+
+export const AnchonCategories = (props:any) => {
+  return (
+    <Anchor onChange={props.setLink} className="ant-anchor-tabs">
+    <Tabs activeKey={props.link}>
+      {restaurant_menu.map((category, i) => (
+        <TabPane
+          key={props.generateAnchor(`${category.name}_${i}`)}
+          tab={
+            <AnchorLink
+              href={`#${props.generateAnchor(`${category.name}_${i}`)}`}
+              title={category.name}
+            />
+          }
+        ></TabPane>
+      ))}
+    </Tabs>
+  </Anchor>
+  );
+};
+
