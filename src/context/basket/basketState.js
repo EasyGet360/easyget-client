1c6097b63d10bc5f7f43744c4291483f5e6b0de4
metradev <sergiop.pias@gmail.com>
2021-04-25 14:49:58 +0200

    Create basket State and actions

---
+++ src/context/basket/basketState.js
@@ -0,0 +1,47 @@
+import React, { useReducer } from 'react';
+import BasketContext from './basketContext';
+import BasketReducer from './basketReducer';
+import PropTypes from 'prop-types';
+
+import { ADD_PRODUCT, DELETE_PRODUCT } from '../types';
+
+const BasketState = (props) => {
+  const initialState = {
+    products: [],
+  };
+
+  const [state, dispatch] = useReducer(BasketReducer, initialState);
+
+  const addProduct = (product) => {
+    try {
+      const data = products.push(product);
+      dispatch({ type: ADD_PRODUCT, payload: data });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const deleteProduct = (product) => {
+    try {
+      const data = products.filter((productState) => product.id !== productState.id);
+      dispatch({ type: DELETE_PRODUCT, payload: data });
+    } catch (error) {}
+  };
+
+  return (
+    <BasketContext.Provider
+      value={{
+        products: state.products,
+        addProduct,
+        deleteProduct,
+      }}
+    >
+      <div>{props.children}</div>
+    </BasketContext.Provider>
+  );
+};
+
+BasketState.propTypes = {
+  children: PropTypes.node,
+};
+export default BasketState;
