c912ab00b9aacd45a5d4c9fc4e4d499f295c4dd2
metradev <sergiop.pias@gmail.com>
2021-04-25 14:49:23 +0200

    FEAT: Create Basket reducer

---
+++ src/context/basket/basketReducer.js
@@ -0,0 +1,20 @@
+import { ADD_PRODUCT, DELETE_PRODUCT } from '../types';
+
+export default (state, action) => {
+  const { payload, type } = action;
+
+  switch (type) {
+    case ADD_PRODUCT:
+      return {
+        ...state,
+        products: payload,
+      };
+    case DELETE_PRODUCT:
+      return {
+        ...state,
+        products: payload,
+      };
+    default:
+      return state;
+  }
+};
