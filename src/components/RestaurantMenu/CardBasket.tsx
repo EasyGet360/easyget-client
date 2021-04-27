4db209a72cfaea57a60594cf0dcab18b7ba137b3
metradev <sergiop.pias@gmail.com>
2021-04-27 16:43:01 +0200

    FEAT: Create a cardbasket component to show products

---
+++ src/components/RestaurantMenu/CardBasket.tsx
@@ -0,0 +1,30 @@
+import React, { useState, useContext } from 'react';
+import BasketContext from '../../context/basket/basketContext';
+import { CloseOutlined } from '@ant-design/icons';
+import { Card, Row, Col, Typography, Modal, Button, Tooltip } from 'antd';
+import { ProductBasket } from '../../models/types';
+const { Title } = Typography;
+
+export const CardBasket: React.FunctionComponent<ProductBasket> = (product) => {
+  const { deleteProduct } = useContext(BasketContext);
+  return (
+    <Card
+      title={`${product.howMuch} x ${product.name}`}
+      extra={
+        <>
+          <div>
+            <Button
+              style={{ marginLeft: 10 }}
+              onClick={() => deleteProduct(product.name)}
+              type="primary"
+              icon={<CloseOutlined />}
+            />
+          </div>
+        </>
+      }
+      bodyStyle={{ textAlign: 'center' }}
+    >
+      {product.description}
+    </Card>
+  );
+};
