3ab96dcb149317fc6d5677b39338768c4beb54ac
metradev <sergiop.pias@gmail.com>
2021-04-27 16:44:15 +0200

    FEAT: Changed ADD_PRODUCT reducer

--- src/context/basket/basketReducer.js
+++ src/context/basket/basketReducer.js
@@ -2,12 +2,11 @@ import { ADD_PRODUCT, DELETE_PRODUCT } from '../types';
 
 export default (state, action) => {
   const { payload, type } = action;
-
   switch (type) {
     case ADD_PRODUCT:
       return {
         ...state,
-        products: payload,
+        products: [...state.products, payload],
       };
     case DELETE_PRODUCT:
       return {
